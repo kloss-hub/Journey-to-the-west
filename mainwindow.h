4957e6942df0e7537a3161268614b8cea99580cd
kloss <2018201777@ruc.edu.cn>
2020-05-27 14:36:28 +0800

    最初版本

--- mainwindow.h
+++ mainwindow.h
@@ -34,15 +34,14 @@ private:
     QVector <Evil *> Evilvec;//存放妖怪的数组
     QVector <Eudemon *> Eudemonvec;//存放守护者的数组
     QVector<Point *> path;//移动路径
-    void paintEvent(QPaintEvent *);         //绘图事件
-    void mousePressEvent(QMouseEvent *);    //鼠标点击事件
-    void DrawMapArr(QPainter&);             //画地图函数
+    void paintEvent(QPaintEvent *);//绘图事件
+    void mousePressEvent(QMouseEvent *);//鼠标点击事件
+    void DrawMapArr(QPainter&);//画地图函数
     void Drawlife(QPainter&,int x,int y);
-//    void DrawSelectionBox(QPainter&);       //用于画出选择框
     int hurt(Evil *);//  判断怪物是否伤害到唐僧
     int Evil_dead(Evil *E);//判断妖怪是否死亡
-    void DrawEudemon(QPainter&);       //画出防御塔
-    void DrawEvil(QPainter&);            //画出怪物
+    void DrawEudemon(QPainter&);
+    void DrawEvil(QPainter&);//画出怪物
     int money=100; //金币
     int evil_count=0;//妖怪数量
     int life=100;  //唐僧的剩余生命
